a6d319c5e3b77dfc558d1b9e1386b9e7dccae843
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-07-21 23:23:27 +0900

    complete 11382

--- 2588.js
+++ 2588.js
@@ -1,14 +1,7 @@
 let fs = require('fs');
-let input = fs.readFileSync('./dev/stdin').toString().trim().split('\n');
+let input = fs.readFileSync('./dev/stdin').toString().trim().split(' ');
 
 let a = +input[0];
-let b = input[1];
-
-let first = a * b[0];
-let second = a * b[1];
-let third = a * b[2];
-
-console.log(third);
-console.log(second);
-console.log(first);
-console.log(third + second * 10 + first * 100);
+let b = +input[1];
+let c = +input[2];
+console.log(a + b + c);
