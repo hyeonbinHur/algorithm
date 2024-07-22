c7109b4ab0fdf33f07e7ec301d59beaa523913ba
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-07-22 18:52:27 +0900

    2438

---
+++ basics/2438.js
@@ -0,0 +1,12 @@
+const input = require('fs')
+    .readFileSync('./dev/stdin.txt')
+    .toString()
+    .trim()
+    .split(' ');
+
+for (let i = 1; i <= +input[0]; i++) {
+    for (let j = 0; j < i; j++) {
+        process.stdout.write('*');
+    }
+    console.log('');
+}
