5a63612c10b1ab957de172ce1b5195aa7af0a12c
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-07-30 00:02:26 +0900

    algo

--- basics/2231.js
+++ basics/2231.js
@@ -10,11 +10,13 @@ let ans = 0;
 for (let i = 0; i < goal + 1; i++) {
     //본인 더하기 자릿수 합
     let current = i.toString();
-    current =
-        Array.from(current).reduce((prev, next) => prev + Number(next), 0) + i;
+    current = Array.from(current).reduce(
+        (prev, next) => prev + Number(next),
+        0
+    );
 
-    if (+current === goal) {
-        ans = current;
+    if (current + i === goal) {
+        ans = i;
         break;
     }
 }
