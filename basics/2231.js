a4a5ba3157d11929248269521556b5dae1889da2
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-07-29 23:59:17 +0900

    algo

---
+++ basics/2231.js
@@ -0,0 +1,22 @@
+const input = require('fs')
+    .readFileSync('./dev/stdin.txt')
+    .toString()
+    .trim()
+    .split('\n');
+
+const goal = +input[0];
+let ans = 0;
+
+for (let i = 0; i < goal + 1; i++) {
+    //본인 더하기 자릿수 합
+    let current = i.toString();
+    current =
+        Array.from(current).reduce((prev, next) => prev + Number(next), 0) + i;
+
+    if (+current === goal) {
+        ans = current;
+        break;
+    }
+}
+
+console.log(ans);
