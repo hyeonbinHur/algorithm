2e183005a1a847596f95add1197dd053a2ddb0df
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-08-30 13:35:01 +0900

    algo bt

---
+++ backtracking/15656.js
@@ -0,0 +1,32 @@
+const input = require('fs')
+    .readFileSync('./dev/stdin.txt')
+    .toString()
+    .trim()
+    .split('\n')
+    .map((e) => e.replace('\r', ''))
+    .map((e) => e.split(' '))
+    .map((e) => e.map((e2) => Number(e2)));
+
+const [n, m] = input[0];
+const nums = input[1].sort((a, b) => a - b);
+const arr = Array(m + 1).fill(0);
+const used = Array(n + 1).fill(false);
+const result = [];
+
+const backTrackin = (k) => {
+    if (k === m) {
+        let current = [];
+        for (let i = 0; i < k; i++) {
+            current.push(arr[i]);
+        }
+        result.push(current);
+        return;
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        arr[k] = nums[i];
+        backTrackin(k + 1);
+    }
+};
+backTrackin(0);
+console.log(result.map((e) => e.join(' ')).join('\n'));
