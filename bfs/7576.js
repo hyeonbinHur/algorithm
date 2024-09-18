455cce9cdc31f4c3b555a2e09efe650fd888b689
(max) hyeon bin <uncle.hyeonb@gmail.com>
2024-09-19 00:34:54 +0900

    algo bfs

--- bfs/7576.js
+++ bfs/7576.js
@@ -17,39 +17,19 @@ const dir = [
     [-1, 0],
 ];
 
-let visited = Array.from({ length: graph.length }, () =>
-    Array(graph[0].length).fill(false)
-);
-
-const base = [];
-
-for (let i = 0; i < graph[0].length; i++) {
-    for (let j = 0; j < graph.length; j++) {
-        if (graph[j][i] === 1) {
-            base.push([j, i, 1]);
+const bfs = () => {
+    const queue = [];
+    for (let i = 0; i < graph[0].length; i++) {
+        for (let j = 0; j < graph.length; j++) {
+            if (graph[j][i] === 1) {
+                queue.push([j, i, 1]);
+            }
         }
     }
-}
+    let head = 0;
+    while (queue.length > head) {
+        const coordinate = queue[head++];
 
-const dayOne = () => {
-    for (let i = 0; i < base.length; i++) {
-        visited = Array.from({ length: graph.length }, () =>
-            Array(graph[0].length).fill(false)
-        );
-        for (let j = 0; j < base.length; j++) {
-            let [startY, startX, day] = base[j];
-            visited[startY][startX] = true;
-        }
-        const [startY, startX, day] = base[i];
-
-        bfs(startY, startX, 1);
-    }
-};
-
-const bfs = (row, col, day) => {
-    const queue = [[row, col, day]];
-    while (queue.length) {
-        const coordinate = queue.shift();
         dir.map((e) => {
             const xPos = e[1] + coordinate[1];
             const yPos = e[0] + coordinate[0];
@@ -59,11 +39,10 @@ const bfs = (row, col, day) => {
                 yPos >= 0 &&
                 xPos < graph[0].length &&
                 yPos < graph.length &&
-                visited[yPos][xPos] === false &&
+                graph[yPos][xPos] === 0 &&
                 graph[yPos][xPos] !== -1
             ) {
                 queue.push([yPos, xPos, currentDate + 1]);
-                visited[yPos][xPos] = true;
                 if (graph[yPos][xPos] === 0) {
                     graph[yPos][xPos] = currentDate + 1;
                 } else if (
@@ -79,8 +58,11 @@ const bfs = (row, col, day) => {
         });
     }
 };
-dayOne();
+
+bfs();
+
 const exists = graph.some((row) => row.includes(0));
+
 if (exists) {
     console.log(-1);
 } else {
