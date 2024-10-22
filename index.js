8c309619125f08ad3f7ded9bfac9b3392d7dde56
Hur Hyeon Bin (Max) <160996936+hyeonbinHur@users.noreply.github.com>
2024-10-22 17:40:26 +0900

    algo tree

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const lc = [1, 3, 0, 0, 0, 0];
-const rc = [2, 4, 5, 0, 0, 0];
-const arr = "aababa";
-let ans = [];
-const postoder = (cur) => {
-  if (lc[cur] !== 0) postoder(lc[cur]);
-  if (rc[cur] !== 0) postoder(rc[cur]);
-  ans.push(arr[cur]);
-};
-for (let i = 0; i < arr.length; i++) {
-  ans = [];
-  postoder(i);
-  console.log(ans.join(""));
+let n = 181;
+
+let setBitCount = 0;
+while (n !== 0) {
+  console.log(n);
+  console.log(n.toString(2));
+  console.log(n - 1);
+  console.log((n - 1).toString(2));
+  n &= n - 1;
+  console.log(n);
+  console.log("---");
+  setBitCount++;
 }
+console.log(setBitCount);
---
+++ tree/1240.js
@@ -0,0 +1,46 @@
+const input = require("fs")
+  .readFileSync("./dev/stdin.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => e.split(" "))
+  .map((e) => e.map((e2) => Number(e2)));
+
+const [n, m] = input.shift();
+
+const ans = () => {
+  const queries = input.splice(input.length - m, m);
+  const tree = Array.from({ length: n + 1 }, () => []);
+  for (let i = 0; i < input.length; i++) {
+    const [s, e, d] = input[i];
+    tree[e].push([s, d]);
+    tree[s].push([e, d]);
+  }
+  const result = [];
+  const bfs = (start, end) => {
+    const visit = Array(n + 1).fill(0);
+    const q = [[start, 0]];
+    visit[start] = 1;
+
+    while (q.length) {
+      const [node, dist] = q.shift();
+      if (node === end) {
+        result.push(dist);
+        break;
+      }
+      for (let i = 0; i < tree[node].length; i++) {
+        const [cur, dist2] = tree[node][i];
+        if (visit[cur] === 0) {
+          q.push([cur, dist + dist2]);
+          visit[cur] = 1;
+        }
+      }
+    }
+  };
+  for (let i = 0; i < queries.length; i++) {
+    const [e, s] = queries[i];
+    bfs(e, s);
+  }
+  console.log(result.join("\n"));
+};
+ans();
